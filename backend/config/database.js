9ee910912dae9e6411acd918417ebf9c72e5095a
Roshan-trial <iambackganesha@gmail.com>
2026-05-06 10:48:27 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 function connect() {
-    mongoose.connect(process.env.mongodb_url).then(() => {
+    mongoose.connect("mongodb+srv://studymork:HJzvW05oAr9aCOlm@cluster0.crvplaq.mongodb.net/lmsProject").then(() => {
         console.log('Connected to the database');
     }).catch((err) => {
         console.log('Error connecting to the database', err);
